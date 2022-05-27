de6bc4b51b3dde6958383d7bd0a6f3fa2b96b532
Artem <sploditelartem@gmail.com>
2022-05-27 16:00:01 +0300

    modificate modal

--- js/script.js
+++ js/script.js
@@ -94,14 +94,18 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
       modal = document.querySelector('.modal'),
       modalCloseBtn = document.querySelector('[data-close]');
 
-  
+
+function openModal () {
+   modal.classList.add('show');
+   modal.classList.remove('hide');
+   document.body.style.overflow = 'hidden';
+   clearInterval(modalTimerID);
+} 
+
 modalTrigger.forEach( btn => {
-   btn .addEventListener('click', () => {
-      modal.classList.add('show');
-      modal.classList.remove('hide');
-      document.body.style.overflow = 'hidden';
-   });
+   btn .addEventListener('click', openModal);
 });
+
 
 function closeModal(){
    modal.classList.add('hide');
@@ -123,4 +127,13 @@ document.addEventListener('keydown', (e) =>{
    }
 }); 
 
+const modalTimerID = setTimeout(openModal, 4000);
+
+function showModalByScroll (){
+   if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+   }
+}
+window.addEventListener('scroll', showModalByScroll);
 });
